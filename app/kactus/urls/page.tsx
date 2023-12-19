bdedaaf3c81772c6112b1aceaf7df915f4a560a0
lolpro11 <lolpro11lolx@gmail.com>
2023-12-18 20:07:01 -0800

    Changes part 2

--- app/kactus/urls/page.tsx
+++ app/kactus/urls/page.tsx
@@ -4,13 +4,9 @@ import {Checkbox} from "@nextui-org/react";
 export default function Page() {
     return (
         <div className="flex flex-col items-center justify-center divide-y divide-solid">
-            <section>
-                <div className="flex items-center relative pt-32 pb-12 md:pt-40 md:pb-16">
-                    <div className="flex gap-4">
-                        <Checkbox defaultSelected>Option</Checkbox>
-                    </div> 
-                </div>
-            </section>
+            <div className="flex gap-4 items-center relative pt-32 pb-12 md:pt-40 md:pb-16">
+                <Checkbox defaultSelected>Option</Checkbox>
+            </div>
         </div>
     )
 }
